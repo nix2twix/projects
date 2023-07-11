550aecef26ee0786df9e87f42c2cbaf44c72c385
nix2twix <121345489+nix2twix@users.noreply.github.com>
2023-07-11 19:26:38 +0300

    GUI added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,64 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MCApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection sqlConnection = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["databaseKey"].ConnectionString);
+            
+            sqlConnection.Open();
+        }
+
+        private void ButtonInsertNewService_Click(object sender, RoutedEventArgs e)
+        {
+            string name = serviceNameTextBox.Text;
+            string minCount = minCountTextBox.Text;
+            string maxCount = maxCountTextBox.Text;
+            string measure = measureTextBox.Text;
+            int idMC = int.Parse(idMCTextBox.Text);
+
+            SqlCommand command = new SqlCommand("INSERT INTO Services (name, minCount, maxCount, measure, idMC)" 
+                + $"VALUES (N'{name}', N'{minCount}', N'{maxCount}', N'{measure}', '{idMC}')", sqlConnection);
+
+            MessageBox.Show(command.ExecuteNonQuery().ToString() + " lines has been executed successfully!");
+        }
+
+        private void DeleteServiceByID_Click(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(serviceIDToDelTextBox.Text);
+
+            SqlCommand command = new SqlCommand("DELETE FROM Services"
+                + $"WHERE id={id}", sqlConnection);
+
+            MessageBox.Show(command.ExecuteNonQuery().ToString() + " lines has been executed successfully!");
+        }
+
+        private void ChangeServiceID_Click(object sender, RoutedEventArgs e)
+        {
+            string name = serviceNameTextBox.Text;
+            string minCount = minCountTextBox.Text;
+            string maxCount = maxCountTextBox.Text;
+            string measure = measureTextBox.Text;
+            int idMC = int.Parse(idMCTextBox.Text);
+
+            SqlCommand command = new SqlCommand("UPDATE Services"
+                + $"SET (N'{name}', N'{minCount}', N'{maxCount}', N'{measure}', '{idMC}')", sqlConnection);
+
+            MessageBox.Show(command.ExecuteNonQuery().ToString() + " lines has been executed successfully!");
+
         }
     }
 }
--- obj/Debug/MCApp_Content.g.i.cs
+++ obj/Debug/MCApp_Content.g.i.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("database.mdf")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("database_log.ldf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mcservicecatalog.mdf")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mcservicecatalog_log.ldf")]
 
 
